99111a3522f313165860bfc4a947cc787ee9187f
Nick Hester <nickhstr@gmail.com>
2016-03-11 23:28:50 -0800

    Minify js and css for pizza.html

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,30 @@ var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
 	imagemin = require('gulp-imagemin');
 
-gulp.task('scripts', function() {
+gulp.task('scriptMain', function() {
 	gulp.src('js/*.js')
 		.pipe(uglify())
-	.pipe(rename('perfmatters.min.js'))
-		.pipe(gulp.dest('js/'));
+		.pipe(rename('perfmatters.min.js'))
+		.pipe(gulp.dest('js'));
 });
 
-gulp.task('styles', function() {
+gulp.task('scriptViews', function() {
+	gulp.src('views/js/*.js')
+		.pipe(uglify())
+		.pipe(rename('main.min.js'))
+		.pipe(gulp.dest('views/js'));
+});
+
+gulp.task('stylesMain', function() {
 	gulp.src('css/*.css')
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('css/minCSS'));
+});
+
+gulp.task('stylesViews', function() {
+	gulp.src('views/css/*.css')
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('views/css/minCSS'));
 });
 
 gulp.task('images', function() {
@@ -27,8 +40,10 @@ gulp.task('images', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('js/*.js', ['scripts']);
-	gulp.watch('css/*.css', ['styles']);
+	gulp.watch('js/*.js', ['scriptMain']);
+	gulp.watch('views/js/*.js', ['scriptViews']);
+	gulp.watch('css/*.css', ['stylesMain']);
+	gulp.watch('views/css/*.css', ['stylesViews']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['scriptMain', 'scriptViews', 'stylesMain', 'stylesViews', 'watch']);
